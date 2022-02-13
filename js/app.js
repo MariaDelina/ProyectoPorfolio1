a9f2457dc5d756e97d85d89489749a7a6ed6fad2
Delina Trentin <sdjhj@hotmail.com>
2022-02-13 06:53:20 -0300

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('body').on({ 'mousemove' : function( e ){
+        
+        let clientX = e.originalEvent.clientX
+        let clientY = e.originalEvent.clientY
+        $('#cursor').css({
+            'left' : (clientX - 40) + 'px',
+            'top' : (clientY - 40) + 'px'
+        })    
+    }})
+
+})
